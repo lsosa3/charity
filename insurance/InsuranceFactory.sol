307152c80523067018f548a69bb371b15cc5d70f
Lucas <lucas.sosa@bairesdev.com>
2022-03-17 23:44:41 -0400

    Lab and client SC and create functions

--- insurance/InsuranceFactory.sol
+++ insurance/InsuranceFactory.sol
@@ -40,26 +40,26 @@ contract InsuranceFactory is BasicOperations {
     // Mappings and arrays
 
     // Mappings address to client struct
-    mapping(address => client) public MappingCustomers;
+    mapping(address => client) public MappingClients;
     // Mappings service name to service struct
     mapping(string => service) public MappingServices;
     // Mapping address to lab
     mapping(address => lab) public MappingLab;
 
 
-    // Array to store customers
-    address [] Customers;
+    // Array to store clients address
+    address [] Clients;
     // Array to store service names
     string [] private ServicesNames;
     // Arry to store labs address
     address [] LabsAddress;
 
-    function CheckOnlyInsured(address _insuredAddress) public view {
-        require(MappingCustomers[_insuredAddress].ClientAuthorization == true, "Client not authorized, please check");
+    function CheckOnlyClient(address _insuredAddress) public view {
+        require(MappingClients[_insuredAddress].ClientAuthorization == true, "Client not authorized, please check");
     }
     // Modifiers to apply restrictions on insured and insurance
     modifier OnlyInsured(address _insuredAddress) {
-        CheckOnlyInsured(_insuredAddress);
+        CheckOnlyClient(_insuredAddress);
         _;
     }
 
@@ -68,8 +68,8 @@ contract InsuranceFactory is BasicOperations {
         _;
     }
 
-    modifier IsuredOrInsurance(address _insuredAddress, address _inCommingAddress) {
-        require((MappingCustomers[_insuredAddress].ClientAuthorization == true && _insuredAddress == _inCommingAddress) || (InsuranceCompany == _inCommingAddress), "Only insured or insurance company are authorized");
+    modifier ClientOrInsurance(address _clientAddress, address _inCommingAddress) {
+        require((MappingClients[_clientAddress].ClientAuthorization == true && _clientAddress == _inCommingAddress) || (InsuranceCompany == _inCommingAddress), "Only client or insurance company are authorized");
         _;
     }
 
@@ -81,4 +81,62 @@ contract InsuranceFactory is BasicOperations {
     event RemoveClientEvent(address);
     event ServiceCreatedEvent(string, uint256);
     event RemoveServiceEvent(string);
+
+
+    function CreateLab() public {
+        LabsAddress.push(msg.sender);
+        address labAddress = address(new Lab(msg.sender, Insurance));
+        MappingLab[msg.sender] = lab(labAddress, true);
+        emit LabCreatedEvent(msg.sender, labAddress);
+    }
+
+    function CreateClientContract() public {
+        Clients.push(msg.sender);
+        address clientAddress = address(new InsuranceHealthRecord(msg.sender, token, Insurance, InsuranceCompany));
+        MappingClients[msg.sender] = client(msg.sender, true, clientAddress);
+        emit NewClientEvent(msg.sender, clientAddress);
+    }
+
+    function showLabs() public view OnlyInsurance(msg.sender) returns (address [] memory) {
+        return LabsAddress;
+    }
+
+    function showClients() public view OnlyInsurance(msg.sender) returns (address [] memory) {
+        return Clients;
+    }
+}
+
+contract InsuranceHealthRecord is BasicOperations {
+
+    enum State { available, notAvailable }
+
+    struct Owner {
+        address ownerAddress;
+        uint balance;
+        State state;
+        IERC20 tokens;
+        address insurance;
+        address payable insuranceCompany;
+    }
+
+    Owner owner;
+
+    constructor (address _owner, IERC20 _token, address _insurance, address payable _insuranceCompany) public {
+        owner.ownerAddress = _owner;
+        owner.balance = 0;
+        owner.state = State.available;
+        owner.tokens = _token;
+        owner.insurance = _insurance;
+        owner.insuranceCompany = _insuranceCompany;
+    }
+}
+
+contract Lab {
+
+    address public labAddress;
+    address insurance;
+    constructor (address _senderAddress, address _insurance) public {
+        labAddress = labAddress;
+        insurance = _insurance;
+    }
 }
