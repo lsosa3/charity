a0c2d0aeab259888dd961dbfb0a0132df770ae3c
Lucas <lucas.sosa@bairesdev.com>
2022-04-07 12:52:13 -0400

    Services management, tokens management and Lab

--- insurance/InsuranceFactory.sol
+++ insurance/InsuranceFactory.sol
@@ -121,10 +121,63 @@ contract InsuranceFactory is BasicOperations {
         return history;
     }
 
-    function unsubscribeClient(address _clientAddress) public OnlyInsurance(msg.sender) {
+    function UnsubscribeClient(address _clientAddress) public OnlyInsurance(msg.sender) {
         MappingClients[_clientAddress].ClientAuthorization = false;
         InsuranceHealthRecord(MappingClients[_clientAddress].ClientContract).Unsubscribe;
         emit UnsubscribeClientEvent(_clientAddress);
+    }
+
+    function NewService(string memory _serviceName, uint _servicePrice) public OnlyInsurance(msg.sender) {
+        MappingServices[_serviceName] = service(_serviceName, _servicePrice, true);
+        ServicesNames.push(_serviceName);
+        ServiceCreatedEvent(_serviceName, _servicePrice);
+    }
+
+    function RemoveService(string memory _serviceName) public OnlyInsurance(msg.sender) {
+        require(GetServiceStatus(_serviceName) == true, "Service not available");
+        MappingServices[_serviceName].ServiceStatus = false;
+        emit RemoveServiceEvent(_serviceName);
+    }
+
+    function GetServiceStatus(string memory _serviceName) public view returns (bool) {
+        return MappingServices[_serviceName].ServiceStatus;
+    }
+
+    function GetServicePrice(string memory _serviceName) public view returns (uint tokens) {
+        require(GetServiceStatus(_serviceName) == true, "Service not available");
+        return MappingServices[_serviceName].ServiceTokenPrice;
+    }
+
+    //Function to show only active services
+    function ShowActiveServices() public view returns(string [] memory) {
+        string [] memory ActiveServices = new string[](ServicesNames.length);
+        uint counter = 0;
+
+        for(uint i = 0; i < ServicesNames.length; i++) {
+            if(GetServiceStatus(ServicesNames[i]) == true) {
+                ActiveServices[counter] = ServicesNames[i];
+                counter++;
+            }
+        }
+
+        return ActiveServices;
+    }
+
+    function BuyTokens(address _client, uint _numTokens) public payable OnlyInsured(_client) {
+        uint256 balance = BalanceOf();
+        require(_numTokens > 0, "Buy a positive amount of tokens");
+        require(_numTokens <= balance, "Buy less tokens");
+
+        token.transfer(msg.sender, _numTokens);
+        emit PurchasedEvent(_numTokens);
+    }
+
+    function BalanceOf() public view returns(uint256 tokens) {
+        return (token.balanceOf(address(this)));
+    }
+
+    function GenerateTokens(uint _numTokens) public OnlyInsurance(msg.sender) {
+        token.increaseTotalSupply(_numTokens);
     }
 }
 
@@ -173,6 +226,9 @@ contract InsuranceHealthRecord is BasicOperations {
     }
 
     event selfDestructEvent(address);
+    event returnTokensEvent(address, uint);
+    event paidServiceEvent(address, string, uint256);
+    event serviceRequestLab(address, address, string);
 
     function HistoryRequestedServicesLab() public view returns (RequestedServicesLab [] memory) {
         return RequestedServicesLabHistory;
@@ -190,6 +246,45 @@ contract InsuranceHealthRecord is BasicOperations {
         emit selfDestructEvent(msg.sender);
         selfdestruct(msg.sender);
     }
+
+    function BuyTokens(uint _numTokens) payable public OnlyOwner(msg.sender) {
+        require(_numTokens > 0, "Please buy a positive number of tokens");
+        uint cost = CalculateTokenPrice(_numTokens);
+        require(msg.value >= cost, "Buy less tokens or send more eth");
+        uint returnValue = msg.value - cost;
+        msg.sender.transfer(returnValue);
+        InsuranceFactory(owner.insurance).BuyTokens(msg.sender, _numTokens);
+    }
+
+    function BalanceOf() public view OnlyOwner(msg.sender) returns (uint256 _balance) {
+        return (owner.tokens.balanceOf(address(this)));
+    }
+
+    function ReturnTokens(uint _numTokens) public payable OnlyOwner(msg.sender) {
+        require(_numTokens > 0, "Please send a positive number of tokens");
+        require(_numTokens <= BalanceOf(), "You don't have that amout of tokens");
+        owner.tokens.transfer(owner.insurance, _numTokens);
+        msg.sender.transfer(CalculateTokenPrice(_numTokens));
+        emit returnTokensEvent(msg.sender, _numTokens);
+    }
+
+    function RequestService(string memory _serviceName) public OnlyOwner(msg.sender) {
+        require(InsuranceFactory(owner.insurance).GetServiceStatus(_serviceName) == true, "Service not available");
+        uint servicePrice = InsuranceFactory(owner.insurance).GetServicePrice(_serviceName);
+        require(BalanceOf() >= servicePrice, "You don't have enough tokens, please buy more");
+        owner.tokens.transfer(owner.insurance, servicePrice);
+        clientHistory[_serviceName] = RequestedServices(_serviceName,servicePrice,true);
+        paidServiceEvent(msg.sender, _serviceName, servicePrice);
+    }
+
+    function RequestLabService(address _labAddress, string memory _serviceName) public payable OnlyOwner(msg.sender) {
+        Lab labContract = Lab(_labAddress);
+        require(msg.value == labContract.GetServicePrice(_serviceName) * 1 ether, "Operacion invalida");
+        labContract.GiveService(msg.sender, _serviceName);
+        payable(labContract.labAddress()).transfer(labContract.GetServicePrice(_serviceName) * 1 ether);
+        RequestedServicesLabHistory.push(RequestedServicesLab(_serviceName, labContract.GetServicePrice(_serviceName), _labAddress));
+        emit serviceRequestLab(_labAddress, msg.sender, _serviceName);
+    }
 }
 
 contract Lab {
@@ -199,5 +294,13 @@ contract Lab {
     constructor (address _senderAddress, address _insurance) public {
         labAddress = labAddress;
         insurance = _insurance;
+    }
+
+    function GetServicePrice(string memory _serviceName) public view returns (uint) {
+        return 0;
+    }
+
+    function GiveService(address _clientAddress, string memory _serviceName) public {
+
     }
 }
